d51c7b593c18399013acbb7a4cfbaa9099551c3e
youngspring1 <youngspring1@163.com>
2016-11-29 15:00:18 +0800

    snap ok

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 var stream;
-  
+var video = document.querySelector('video'); 
+var button = document.querySelector('button');
+var canvas = document.querySelector('canvas');
+
+
 function hasUserMedia() { 
    //check if the browser supports the WebRTC 
    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || 
@@ -13,42 +17,18 @@ if (hasUserMedia()) {
    //enabling video and audio channels 
    navigator.getUserMedia({ video: true, audio: true }, function (s) { 
       stream = s; 
-      var video = document.querySelector('video'); 
-		
+
       //inserting our stream to the video tag     
       video.src = window.URL.createObjectURL(stream); 
+
    }, function (err) {}); 
 	
 } else { 
    alert("WebRTC is not supported"); 
 }
-  
-btnGetAudioTracks.addEventListener("click", function(){ 
-   console.log("getAudioTracks"); 
-   console.log(stream.getAudioTracks()); 
-});
-  
-btnGetTrackById.addEventListener("click", function(){ 
-   console.log("getTrackById"); 
-   console.log(stream.getTrackById(stream.getAudioTracks()[0].id)); 
-});
-  
-btnGetTracks.addEventListener("click", function(){ 
-   console.log("getTracks()"); 
-   console.log(stream.getTracks()); 
-});
- 
-btnGetVideoTracks.addEventListener("click", function(){ 
-   console.log("getVideoTracks()"); 
-   console.log(stream.getVideoTracks()); 
-});
 
-btnRemoveAudioTrack.addEventListener("click", function(){ 
-   console.log("removeAudioTrack()"); 
-   stream.removeTrack(stream.getAudioTracks()[0]); 
-});
-  
-btnRemoveVideoTrack.addEventListener("click", function(){ 
-   console.log("removeVideoTrack()"); 
-   stream.removeTrack(stream.getVideoTracks()[0]); 
-});
+button.onclick = function() {
+	canvas.getContext("2d").drawImage(video, 0, 0, 320, 240);
+	var img = canvas.toDataURL("png");
+	
+};
